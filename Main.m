cd68a188c41c3d624b707b191c3bd98b28841dc5
L-Phillips831 <loganaphillips831@gmail.com>
2025-07-14 19:51:07 -0500

    Create Main.m

---
+++ Main.m
@@ -0,0 +1,23 @@
+clc, clear, close all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                        %
+%                       AU EAGLE SIZING CODE MAIN                        %
+%                                                                        %   
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Important Variable Definitions and Object Creations
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% Running the EAGLE Solver
