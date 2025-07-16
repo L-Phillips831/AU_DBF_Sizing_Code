1d6bd21703ae9bf151f8ccdf867fe77b4a71f56a
L-Phillips831 <loganaphillips831@gmail.com>
2025-07-15 19:50:08 -0500

    Put cost function for GA in main for easier user access.

--- +optimizer/GA_Optimizer.m
+++ +optimizer/GA_Optimizer.m
@@ -1,12 +1,13 @@
 classdef GA_Optimizer
     % This class delas with the construction and usage of the optimization
-    % using MATLAB's Genetic Algorithm optimization package.
+    % using MATLAB's Genetic Algorithm optimization package. Serves to
+    % simplify the user interface with dealing with the GA optimizer
     
     properties
         pop_size (1,1) uint16       % Population size for GA solver
         generations (1,1) uint16    % Number of generations for GA solver
-        lower_bds  (1,3) double     % Array of lower limits for GA solver
-        upper_bds (1,3) double      % Array of upper limits for GA solver
+        lower_bds double            % Array of lower limits for GA solver
+        upper_bds double            % Array of upper limits for GA solver
     end
     
     methods
@@ -21,17 +22,28 @@ classdef GA_Optimizer
             obj.generations = settings_.generations;
             obj.lower_bds = settings_.lower_bds;
             obj.upper_bds = settings_.upper_bds;
+
+            % Checking user input for parameter vector
+            if (length(obj.lower_bds) ~= length(obj.upper_bds))
+                error('Lower and upper bounds must have the same length. Got %d and %d.', ...
+                length(obj.lower_bds), length(obj.upper_bds));
+            end
+
         end
         
         % Method for running the GA optimization
         % Params:
         %   - obj: the instance of the GA class
+        %   - cost_handle: the handle to the cost function created in the
+        %                   main script. This allows the cost function to be more easily
+        %                   managed by the user since this function defines the parameters
+        %                   for optimization
         % Returns:
         %   - best_score: the minimized cost returned by the genetic
         %                 algorithm
         %   - best_params: the array of optimal params determined by the GA
         %                  solver
-        function [best_score, best_params] = run(obj)
+        function [best_score, best_params] = run(obj, cost_handle)
 
 
             % Options for solver
@@ -39,22 +51,12 @@ classdef GA_Optimizer
 
             % Call the solver
             num_var = length(obj.upper_bds);
-            [best_params, best_score] = ga(@GA_Optimizer.cost_generation,num_var,[],[],[],[],obj.lower_bds, obj.upper_bds,[],ga_opt);
+            [best_params, best_score] = ga(cost_handle,num_var,[],[],[],[],obj.lower_bds, obj.upper_bds,[],ga_opt);
 
         end
 
 
-    end
-    methods (Static)
-
-         function cost = cost_generation(params)
-            T2W = params[1];
-            W_S = params[2];
-            AR = Params[3];
-
-         end
-
-    end
+    end    
     
 end
 
--- Main.m
+++ Main.m
@@ -6,10 +6,16 @@ clc, clear, close all;
 %                                                                        %   
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% Important Variable Definitions and Object Creations
+%% Create the Optimizer Object
+
+ga_settings.lower_bds = zeros([1 3]);                % Set the lower bounds for the optimizer
+ga_settings.upper_bds = [100 100 100];               % Set the upper bounds for the  optimizer
+ga_settings.pop_size = 15;                           % Set the population size for GA optimizer
+ga_settings.generations = 5;                         % Define the number of generations for the GA optimizer
+GA_optimizer = optimizer.GA_Optimizer(ga_settings);  % Create optimizer object       
 
 
-
+[best_score, best_params] = GA_optimizer.run(@EAGLE_Solver);
 
 
 
@@ -21,3 +27,18 @@ clc, clear, close all;
 
 
 %% Running the EAGLE Solver
+
+
+function cost = EAGLE_Solver(vehicle_params)
+
+    % Define Vehicle
+    
+
+
+
+
+
+    % Generate Cost
+    cost = 1/score; % Higher score generates lower cost.
+
+end
