a3235f7963e9a5923fabd0df418daea4968143f4
RGHman <rgholman.2004@gmail.com>
2025-07-22 18:12:51 -0500

    Named Instant Turn
    yay

--- +mission/Turn.m
+++ +mission/Turn.m
@@ -14,12 +14,12 @@ classdef Turn < Mission_Segment
             obj.Property1 = inputArg1 + inputArg2;
         end
         
-        function outputArg = method1(obj,inputArg)
+        function outputArg = InstantaneousTurn(obj,inputArg)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
             outputArg = obj.Property1 + inputArg;
         end
-        
+
         function outputArg = method2(obj,inputArg)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
