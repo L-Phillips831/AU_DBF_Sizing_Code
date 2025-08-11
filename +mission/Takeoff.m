97b4e44a12f24288b19b5923d494e2356ba51e7f
RGHman <rgholman.2004@gmail.com>
2025-08-11 15:18:05 -0500

    Building out segment class functions
    Separated Takeoff and Turn functions into their respective classes.

--- +mission/Takeoff.m
+++ +mission/Takeoff.m
@@ -7,16 +7,132 @@ classdef Takeoff < Mission_Segment
     end
     
     methods
-        function obj = Takeoff(inputArg1,inputArg2)
-            %TAKEOFF Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+        function MissionTable = Takeoff(obj)
+        %TAKEOFF Construct an instance of this class
+        %{
+        Talk with cole about this one
+        - Will have ground run, rotation, transition
+        - Rotation discretized over both vLOF and aoaLOF
+        - transition discretized over aoaLOF to aoaClimb and vLOF to vClimb
+        - Does not account for sloped runway
+        - Need propulsion equations
+        - Need CL calculation from aoa
+        %}
+        % General variables
+            tab  = obj.MissionTable;                                        % Need call
+            Sref = obj.Sref;                                                % Need call
+            k    = obj.k;                                                   % Need call
+            CD0  = obj.CD0;                                                 % Need call
+            rho  = obj.rho;                                                 % Need call
+            mass = tab.mass(end);                                           % Need call
+
+            % define object variables and discretization bounds
+            gFriction = 0; % Need value
+            g = 0; % Need value                                             
+            aoaStart  = 0; % Need value
+            vRot      = 0; % Need value
+            vLOF      = 0; % Need value
+            aoaLOF    = 0; % Need value
+            vClimb    = 0; % Need value
+            aoaClimb  = 0; % Need value
+            numVals   = 0; % Need value
+            numParts  = 3;                                                  % Ground run, rotation, transition
+            Throttle  = 0; % Need vector/value
+
+            % Initialize table variables
+            t = zeros(numParts*numVals, 1);
+            d = zeros(numParts*numVals, 1);
+            v = zeros(numParts*numVals, 1);
+            a = zeros(numParts*numVals, 1);
+            aoa=zeros(numParts*numVals, 1);
+            pUse=zeros(numParts*numVals, 1);
+            thrust=zeros(numParts*numVals, 1);
+            q=zeros(numParts*numVals, 1);
+            CL=zeros(numParts*numVals, 1);
+            CD=zeros(numParts*numVals, 1);
+            L=zeros(numParts*numVals, 1);
+            D=zeros(numParts*numVals, 1);
+            LD=zeros(numParts*numVals, 1);
+            hDot=zeros(numParts*numVals, 1);
+            rho=(tab.rho)*ones(numParts*numVals, 1);                        % Constant
+            h = zeros(numParts*numVals, 1); 
+            Energy = zeros(numVals, 1); %
+
+            % Other initializations
+            dt= zeros(numParts*numVals,1);
+
+            % Ground run
+            v(1:numVals)   = linspace(0, vRot, numVals);                    % Discretization for GR
+            aoa(1:numVals) = aoaStart;                                      % Good
+            h(1:numVals)   = zeros(numVals, 1);                             % Good
+            for i = 1:numVals
+                [thrust(i), pUse(i)] = ThrustCalc(v(i), T_set(i));          % Need function
+                q(i)  = 0.5*rho(i)*v(i)^2;                                  % Good
+                CL(i) = CalcCL(aoa(i));                                     % Need function
+                CD(i) = CD0 + k*CL(i)^2;                                    % Good
+                LD(i) = CL(i)/CD(i);                                        % Good
+                L(i)  = CL*Sref*q(i);                                       % Good
+                D(i)  = (L(i)/LD(i)) + ((mass*g-L(i))*gFriction);           % Good
+                a = (thrust(i) - D(i))/m;                                   % Good
+                if i == 1
+                    dt(1) = 0;                                              % dt starts at 0
+                    t(1)  = 0;                                              % Time starts at 0
+                    d(1)  = 0;                                              % Distance starts at 0
+                else
+                    % dt is dv/a
+                    dt(i)         = (v(i)-v(i-1))/(0.5*(a(i-1)+a(i)));      % Good
+                    t(i)          = t(i-1) + dt(i);                         % Good
+                    d(i)          = 0.5*(v(i-1)+v(i))*dt(i);                % Good
+                    Energy(i) = Energy(i-1) + 0.5*(pUse(i-1)+pUse(i))*dt;   % Good
+                end
+            end
+
+        % Rotation
+            v(numVals+1 : numVals*2) = linspace(vRot, vLOF, numVals);       % Discretized from vRot to vLOF
+            aoa(numVals+1 : numVals*2) = linspace(0, aoaLOF, numVals);      % Assume constant rotation rate?
+            h(numVals+1 : numVals*2) = zeros(numVals, 1);                   % Good
+            for i = numVals+1 : numVals*2
+                [thrust(i), pUse(i)] = ThrustCalc(v(i), Throttle(i));       % Need function
+                q(i)  = 0.5*rho(i)*v(i)^2;                                  % Good
+                CL(i) = CalcCL(aoa(i));                                     % Need function. Changing CL.
+                CD(i) = CD0 + k*CL(i)^2;                                    % Good
+                LD(i) = CL(i)/CD(i);                                        % Good
+                L(i)  = CL*Sref*q(i);                                       % Good
+                D(i)  = (L(i)/LD(i)) + ((mass*g-L(i))*gFriction);           % Good
+                a = (thrust(i) - D(i))/m;                                   % Good
+                dt(i)         = (v(i)-v(i-1))/(0.5*(a(i-1)+a(i)));      % Good
+                t(i)          = t(i-1) + dt(i);                         % Good
+                d(i)          = 0.5*(v(i-1)+v(i))*dt(i);                % Good
+                Energy(i) = Energy(i-1) + 0.5*(pUse(i-1)+pUse(i))*dt;   % Good
+            end
+
+        % Transition
+            v(2*numVals+1 : numVals*3) = linspace(vRot, vLOF, numVals);       % Discretized from vRot to vLOF
+            aoa(2*numVals+1 : numVals*3) = linspace(aoaLOF, aoaClimb, numVals);      % Assume constant rotation rate?
+        for i = 1:numVals
+            h(i) = integrateHDot;
+            logicTRANS()
         end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+
+        % New structure/table
+            Snew.Time = t;
+            Snew.E = Energy;
+            Snew.Power = pUse;
+            Snew.Distance = d;
+            Snew.V = v;
+            Snew.Acceleration = a;
+            Snew.Altitude = h;
+            Snew.hDot = hDot;
+            Snew.Mass = mass*ones(numVals,1);
+            Snew.Thrust = thrust;
+            Snew.q = q;
+            Snew.CL = CL;
+            Snew.CD = CD;
+            Snew.Lift = L;
+            Snew.Drag = D;
+            Snew.LD = LD;
+            Tnew = struct2table(Snew); % Make it a structure
+            MissionTable = [tab; Tnew]; % Concatenate tables
         end
     end
 end
--- +mission/Turn.m
+++ +mission/Turn.m
@@ -7,6 +7,7 @@ classdef Turn < Mission_Segment
     end
     
     methods
+
         function obj = Turn(table)
 
             obj.table = table;
@@ -29,16 +30,184 @@ classdef Turn < Mission_Segment
             table = obj.table;
         end
         
-        function obj = InstantaneousTurn(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            obj.table = obj.Property1 + inputArg;
+        
+        function MissionTable = InstantaneousTurn(obj)
+        % Instantaneous Turn Mission Segment
+
+        % Description: Takes heading change, a flight history table, and a
+        % path label between instantaneous and sustained turns.
+        % How are parameters defined for instantaneous or sustained?
+        % Sustained -> Constant turn rate at a certain load factor.
+        % Instantaneous -> Set throttle and limited by aero/structure.
+
+        % General variables
+            tab  = obj.MissionTable;                                        % Need call
+            Sref = obj.Sref;                                                % Need call
+            k    = obj.k;                                                   % Need call
+            CD0  = obj.CD0;                                                 % Need call
+            rho  = obj.rho;                                                 % Need call
+            mass = tab.mass(end);                                           % Need call
+            g    = obj.g;                                                   % Need call
+
+        % Instantaneous variables
+            lfMax = 0;                                                      % Need call, maximum structural load factor
+
+
+        % Initialize table variables
+            t      = tab.Time(end)*ones(numVals, 1); 
+            d      = tab.Distance*ones(numVals, 1);
+            v      = tab.Velocity*ones(numVals, 1);
+            a      = zeros(numVals, 1);
+            pUse   = zeros(numVals, 1);
+            thrust = zeros(numVals, 1); 
+            q      = zeros(numVals, 1); 
+            CL     = zeros(numVals, 1); 
+            CD     = zeros(numVals, 1); 
+            L      = zeros(numVals, 1); 
+            D      = zeros(numVals, 1); 
+            LD     = zeros(numVals, 1); 
+            hDot   = zeros(numVals, 1);                                     
+            h      = tab.Altitude*ones(numVals, 1);                         
+            Energy = tab.Energy(end)*ones(numVals, 1);                      
+
+
+        % Discretization on heading
+            phi = linspace(0, dPhi, numVals);
+           
+
+        % Instantaneous
+            for i = 1:numVals
+                if i>1
+                    v(i) = v(i-1) + a(i-1)*dt;
+                end
+                % If i>1, calculate velocity
+                q(i) = 0.5*rho*v(i)^2;
+                LmaxAero = CLmax*q(i)*Sref;
+                LmaxStructure = lfMax*mass*g;
+                L(i) = min([LmaxAero, LmaxStructure]);                      % Constrained by aero or structure
+                aCentr = L(i)/mass;                                         % bank at 90 deg)
+                CL(i) = L(i)/(Sref*q(i));
+                CD(i) = CD0 + k*CL(i)^2;
+                LD(i) = CL(i)/CD(i);
+                D(i) = L(i)/LD(i);
+                [thrust(i), pUse(i)] = PropulsionCalc(v(i), T_set);         % Need function
+                a(i) = (thrust(i) - D(i))/mass;
+                r = v(i)^2 / aCentr;
+                omega = v(i)/r;
+                if i>1
+                    Energy(i) = Energy(i-1) + pUse*dt;
+                    dt = (phi(i)-phi(i-1))/omega;
+                    t(i) = t(i-1) + dt;
+                    d(i) = d(i-1) + (phi(i) - phi(i-1))*r;                  % Small angle approx
+                    hDot(i) = g*(t(i) - t(1));
+                    h(i) = h(1) - 0.5*g*(t(i) - t(1));                      
+                end                
+            end
+            
+
+        % New structure/table
+            Snew.Time = t;
+            Snew.E = Energy;
+            Snew.Power = pUse;
+            Snew.Distance = d;
+            Snew.V = v;
+            Snew.Acceleration = a;
+            Snew.Altitude = h;
+            Snew.hDot = hDot;
+            Snew.Mass = mass*ones(numVals,1);
+            Snew.Thrust = thrust;
+            Snew.q = q;
+            Snew.CL = CL;
+            Snew.CD = CD;
+            Snew.Lift = L;
+            Snew.Drag = D;
+            Snew.LD = LD;
+            Tnew = struct2table(Snew); % Make it a structure
+            MissionTable = [tab; Tnew]; % Concatenate tables
         end
 
-        function obj = SustainedTurn(obj,inputArg)
-            %METHOD1 Summary of this method goes here
+
+
+        function MissionTable = SustainedTurn(obj)
+            % Sustained Turn Mission Segment
             %   Detailed explanation goes here
-            obj.table = obj.Property1 + inputArg;
+
+            % General variables
+            tab  = obj.MissionTable;                                        % Need call
+            Sref = obj.Sref;                                                % Need call
+            k    = obj.k;                                                   % Need call
+            CD0  = obj.CD0;                                                 % Need call
+            rho  = obj.rho;                                                 % Need call
+            mass = tab.mass(end);                                           % Need call
+            g    = obj.g;                                                   % Need call
+
+
+            % Sustained variables
+            lf = 0;                                                         % Need call, sustained load factor
+
+
+            % Initialize table variables
+            t      = tab.Time(end)*ones(numVals, 1); 
+            d      = tab.Distance*ones(numVals, 1);
+            v      = tab.Velocity*ones(numVals, 1);
+            a      = zeros(numVals, 1);
+            pUse   = zeros(numVals, 1);
+            thrust = zeros(numVals, 1); 
+            q      = zeros(numVals, 1); 
+            CL     = zeros(numVals, 1); 
+            CD     = zeros(numVals, 1); 
+            L      = zeros(numVals, 1); 
+            D      = zeros(numVals, 1); 
+            LD     = zeros(numVals, 1); 
+            hDot   = zeros(numVals, 1);                                     
+            h      = tab.Altitude*ones(numVals, 1);                         
+            Energy = tab.Energy(end)*ones(numVals, 1);                      
+
+
+            % Discretization on heading
+            phi = linspace(0, dPhi, numVals);
+
+
+            % Sustained
+            for i = 1:numVals
+                q(i) = 0.5*rho*v(i)^2;
+                L(i) = lf*mass*g;
+                aCentr = sqrt(lf^2 - 1);
+                r = v(i)^2 / aCentr;
+                CL(i) = L(i)/(Sref*q(i));
+                CD(i) = CD0 + k*CL(i);
+                LD(i) = CL(i)/CD(i);
+                D(i) = L(i)/LD(i);
+                thrust(i) = D(i);                                           
+                propPower = thrust(i)*v(i);                                 % Necessary?
+                pUse = PowerCalc(propPower);                                % Need function. Want v and thrust?
+                if(i>1)
+                    d(i) = d(i-1)+(phi(i)-phi(i-1))*r;                      % Small angle approximation
+                    t(i) = t(i-1)+(phi(i)-phi(i-1))/sqrt(aCentr/r);         
+                    Energy(i) = Energy(i-1) + pUse*(t(i) - t(i-1));
+                end
+            end
+
+            
+            % New structure/table
+            Snew.Time = t;
+            Snew.E = Energy;
+            Snew.Power = pUse;
+            Snew.Distance = d;
+            Snew.V = v;
+            Snew.Acceleration = a;
+            Snew.Altitude = h;
+            Snew.hDot = hDot;
+            Snew.Mass = mass*ones(numVals,1);
+            Snew.Thrust = thrust;
+            Snew.q = q;
+            Snew.CL = CL;
+            Snew.CD = CD;
+            Snew.Lift = L;
+            Snew.Drag = D;
+            Snew.LD = LD;
+            Tnew = struct2table(Snew); % Make it a structure
+            MissionTable = [tab; Tnew]; % Concatenate tables
         end
     end
 end
