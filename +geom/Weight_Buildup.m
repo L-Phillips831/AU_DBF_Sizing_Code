33b144fdb52b81e783ef588f2fcea911b13887a6
RGHman <rgholman.2004@gmail.com>
2025-08-21 00:00:05 -0500

    Update Weight Buildup
    Made an aero surface function to estimate wing and tail sections.

--- +geom/Weight_Buildup.m
+++ +geom/Weight_Buildup.m
@@ -13,88 +13,30 @@ buildup, for now, is suited for conventional configuration aircraft
 % Need Logan's MATLAB expertise to format
 
 %% Densities
-rhoCF = 0; % [g/m^3]
-rhoBalsa = 0; % [g/m^3]
-rhoPly = 0; % [g/m^3]
-rhoMonokote = 0; % A 2D Density [g/m^2]
+rho.CF = 0; % [g/m^3]
+rho.Balsa = 0; % [g/m^3]
+rho.Ply = 0; % [g/m^3]
+rho.Monokote = 0; % A 2D Density [g/m^2]
 
 %% Adjustments
-% Description: Non-dimensional constants determined by historical and experimental averages
-% of real components
-adjRib = 0; % Ratio of rib area to MAC^2
-adjMonokote = 0; % Ratio of Monokote area used to wetted area
-adjLeading = 0; % Ratio of leading edge skin arc length to MAC
-adjAdhesion = 0; % Ratio of adhesion skin arc to MAC
-adjWinglet = 0; % Ratio of winglet area to MAC^2
-adjRibControl = 0; % Ratio of control surface rib area to MAC^2
+% Description: Non-dimensional constants determined by historical and 
+% experimental averages of real components. Should roughly apply to all
+% aerodynamic surfaces (wing, tail)
+adj.Rib = 0; % Ratio of rib area to MAC^2
+adj.Monokote = 0; % Ratio of Monokote area used to wetted area
+adj.Leading = 0; % Ratio of leading edge skin arc length to MAC
+adj.Adhesion = 0; % Ratio of adhesion skin arc to MAC
+adj.Winglet = 0; % Ratio of winglet area to MAC^2
+adj.RibControl = 0; % Ratio of control surface rib area to MAC^2
 
-%% Wing
-% Inputs:
-% - MAC
-% - numPlyRib
-% - numBalsaRib
-% - span
-% - wettedArea
-% - loadFactor (atMTOW)
-% - MTOW
-% - nacelleMass
-% - wingletsBool
+%% Component Mass Calculations
+loadWing = 0.5 * lfMTOW * weight;
+loadHorzTail = 0;
+loadVertTail = 0;
 
-% Function Finds for Wing:
-% - spaceIn / spaceOut
-fsCF = 5; % Wing spar stress factor of safety
-sigmaCF = 0; % N/m^2 normal stress, from Rockwest Composites Site
-
-mSpec = @(y) 4*y/(pi*b) * sqrt(1 - (2*y/b)^2);
-moment = I*weight*lfMTOW;
-rOuter = 0; % Implement bisection solver here
-xcSpar = pi*(rOuter^2 - (rOuter - thicknessSpar)^2);
-
-% Components, discounting quantity and variants/configs:
-
-% Ribs = MAC^2 * adjustment of area * density
-rib = MAC^2 * adjRib; % Independent of density and quantity
-adjPlyRib = numPlyRib * rhoPly;
-adjBalsaRib = numBalsaRib * rhoBalsa;
-ribs = rib*(adjPlyRib + adjBalsaRib);
-
-% Wing Spar = span*f(total estimated normal stress * FS)
-spar = span * crossSectionSparWing * rhoCF;
-
-% Stringers
-stringer = 0.003175^2 * span * rhoBalsa; % Independent of quantity
-stringers = numStringers * stringer;
-
-% Monokote
-monokote = wettedArea * adjMonokote * rhoMonokote;
-
-% LE Skin
-skinLeading = span * MAC * adjLeading;
-
-% Adhesion Skin
-skinAdhesion = 4 * MAC * adjAdhesion * rhoBalsa * 0.003175/4 * (spaceIn + spaceOut);
-
-% Winglets
-winglets = wingletsBool * MAC^2 * adjWinglet * rhoPly;
-
-% Control Surface Ribs
-ribControl = MAC^2 * adjRibControl; % Independent of density and quantity
-adjControlHorn = numPlyRib * rhoPly;
-adjBalsaControl = numBalsaRib * rhoBalsa;
-ribs = rib*(adjControlHorn + adjBalsaControl);
-
-% Control Surface Spar
-sparControl
-
-% Control Surface Skin
-
-
-% Control Surface Stringers
-
-
-wingMass = ribs + spar + stringers + monokote + nacelleMass + skinLeading + skinAdhesion + winglets;
-wingControlsMass = ribsControl + sparControl + skinControl + stringersControl;
-wingMass = wingMass + wingControlsMass;
+massWing = CalcWing (MAC_Wing, spanWing, adj, rho, numWing, loadWing);
+massHorzTail = CalcWing (MAC_HorzTail, span, adj, rho, num, loadHorzTail);
+massVertTail = CalcWing (MAC_VertTail, span, adj, rho, num, loadVertTail);
 
 %% Tail
 
@@ -108,6 +50,120 @@ wingMass = wingMass + wingControlsMass;
 % Total Mass = Wing + HTail + VTail + Fuselage + Powerplant + Mission
 % Payload
 
+function massWing = CalcWing (MAC, span, adj, rho, num, load)
+%% Wing
+    % Inputs:
+    % - MAC
+    % - numPlyRib
+    % - numBalsaRib
+    % - span
+    % - wettedArea
+    % - loadFactor (atMTOW)
+    % - MTOW
+    % - nacelleMass
+    % - wingletsBool
+
+    % Function Finds for Wing:
+    % - spaceIn / spaceOut
+    fsCF = 5; % Wing spar stress factor of safety
+    sigmaCF = 0; % N/m^2 normal stress, from Rockwest Composites Site
+    thickness = 0;
+
+    mSpec = @(y, b) 4/(pi*b) * sqrt(1 - (2*y/b)^2);
+    mWrapper = @(y) mSpec(y, span);
+    I = integral(mWrapper, 0, b/2);
+    moment = I*load;
+    roWrap = @(rO) CalcRoRemainder (moment, sigmaCF, rO, thickness);
+    rOuter = bisection(roWrap, 1e-6, 0.1, 1e-6, 1000);
+    xcSpar = pi*(rOuter^2 - (rOuter - thicknessSpar)^2);
+
+    % Components, discounting quantity and variants/configs:
+
+    % Ribs
+    rib = MAC^2 * adjRib; % Independent of density and quantity
+    adjPlyRib = numPlyRib * rho.Ply;
+    adjBalsaRib = numBalsaRib * rho.Balsa;
+    ribs = rib*(adjPlyRib + adjBalsaRib);
+
+    % Wing Spar
+    spar = span * xcSpar * rho.CF;
+
+    % Stringers
+    stringer = 0.003175^2 * span * rho.Balsa; % Independent of quantity
+    stringers = num.Stringers * stringer;
+
+    % Monokote
+    monokote = (2.1 * span*MAC) * adj.Monokote * rho.Monokote; % Consider better approximation of wetted area
+
+    % LE Skin
+    skinLeading = span * MAC * adj.Leading;
+
+    % Adhesion Skin
+    skinAdhesion = 4 * MAC * adj.Adhesion * rho.Balsa * 0.003175/4 * (spaceIn + spaceOut);
+
+    % Winglets
+    winglets = 2 * wingletsBool * MAC^2 * adj.Winglet * rho.Ply;
+    
+    % Control Surface Ribs
+    ribControl = MAC^2 * adj.RibControl; % Independent of density and quantity
+    adjControlHorn = num.RibControl * rho.Ply;
+    adjBalsaControl = num.RibHorn * rho.Balsa;
+    ribsControl = ribControl*(adjControlHorn + adjBalsaControl);
+
+    % Control Surface Spar
+    sparControl = MAC * span * adj.SparControl * 0.003175 * rho.Balsa;
+
+    % Control Surface Skin
+    skinControl = span * MAC * adj.LeadingControl;
+
+    % Control Surface Stringers
+    stringerControl = span * 0.003175 * 0.0127; % 
+    stringersControl = num.StringersControl * stringerControl;
+
+    massWing = ribs + spar + stringers + monokote + nacelleMass + skinLeading + skinAdhesion + winglets;
+    massControls = ribsControl + sparControl + skinControl + stringersControl;
+    massWing = massWing + massControls;
+end
+
+
 function remainder = CalcRoRemainder (moment, stress, rO, thickness)
+    % Calculates the difference between a manipulated normal stress due to
+    % bending formula.
+    % moment    : The total moment at the root of an aero surface
+    % stress    : The normal stress a carbon fiber tube can survive
+    % rO        : The outer radius of the carbon fiber tubing
+    % thickness : The thickness of the carbon fiber tubing
+
     remainder = stress*pi/(4*moment) - rO / (rO^4 - (rO-thickness)^4);
+end
+
+
+function root = bisection(f, a, b, tol, maxIter)
+    % Bisection method for root finding
+    % f        : function handle
+    % a, b     : interval endpoints
+    % tol      : tolerance for convergence
+    % maxIter  : maximum iterations
+    
+    if f(a)*f(b) > 0
+        error('f(a) and f(b) must have opposite signs');
+    end
+    
+    for k = 1:maxIter
+        c = (a+b)/2;        % midpoint
+        fc = f(c);
+        
+        if abs(fc) < tol || (b-a)/2 < tol
+            root = c;
+            return;
+        end
+        
+        if f(a)*fc < 0
+            b = c;          % root is in [a,c]
+        else
+            a = c;          % root is in [c,b]
+        end
+    end
+    
+    root = (a+b)/2;
 end
