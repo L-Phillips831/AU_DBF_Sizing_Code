40bc730fc34ee1fb88fe19973a85619695c8d206
L-Phillips831 <loganaphillips831@gmail.com>
2025-08-22 22:21:54 -0500

    Additions to AVL interfacing for lifting surfaces.

--- Vehicle.m
+++ Vehicle.m
@@ -5,8 +5,14 @@ classdef Vehicle
     % the optimizer in order to maximize mission performance
     
     properties
-        components struct                           % struct of components
-        % Propulsion (1,1) powerplant.Propulsion
+        name string
+        components geom.Component                       % struct of components
+        Propulsion (1,1) powerplant.Propulsion
+        CG (3,1) double                                 % X,Y,Z loc of CG
+
+        S_ref (1,1) double
+        c_ref (1,1) double
+        b_ref (1,1) double
 
         mass (1,1) double
         Cl_alpha (1,1) double
@@ -26,6 +32,17 @@ classdef Vehicle
         function obj = Vehicle(params, constraints)
 
         end
+        
+
+        function obj = add_wing()
+
+        end
+
+
+        function obj = add_tail()
+
+        end
+
 
         % Method to get the Cl_alpha of the entire aircraft. Serves as a
         % wrapper function for avl related methods.
@@ -42,19 +59,93 @@ classdef Vehicle
 
     methods (Access = private)
 
-        function filepath = generate_geom_file(obj)
+
+        % Method to generate the complete geom input file for AVL analysis.
+        % Loops through each user added component and adds its
+        % corresponding surface into the file.
+        % Params:
+        %   - current object
+        % Returns:
+        %   - none
+        %
+        function generate_geom_file(obj)
+
+            file_path = "+lib\+AVL\Input\geom_file.avl";
+
+            if exist(file_path, 'file')
+                delete(file_path)
+            end
+
+
+            str = [
+                    "#Mach",...
+                    "0.0",...    
+                    "#IYsym   IZsym   Zsym",...
+                    "0       0       0.0",...
+                    "#Sref    Cref    Bref",...
+                    sprintf("%.1f %.1f %.1f", obj.S_ref, obj.c_ref,  obj.b_ref),...
+                    "#Xref    Yref    Zref",...
+                    sprintf("%.1f %.1f %.1f", obj.CG(1), obj.CG(2), obj.CG(3)),...
+                    ];
+
+            for component = obj.components
+                str = [str, component.convert_to_avl_geom()]; 
+            end
+
+            writelines(str, file_path);
 
         end
 
-        function [filepath, num_cases] = generate_case_file(alpha_range)
+
+
+        function num_cases = generate_case_file(alpha_range, airspeed, file_path)
+
+            file_path = "+lib\+AVL\Input\case_file.run";
+    
+            if exist(file_path, 'file')
+                delete(file_path)
+            end
+
+
+            num_cases = numel(alpha_range);
+
+
+
+
+
+
+
+
+
+            writelines(str, file_path);
 
         end
 
-        function filepath = generate_cmd_file(geom_file, case_file, num_cases, results_file)
+        function saved_files = generate_cmd_file(geom_file, case_file, num_cases, results_file)
+            
+            file_path = "+lib\+AVL\Input\cmd_file.cmd";
+    
+            if exist(file_path, 'file')
+                delete(file_path)
+            end
+
+
+
+
+
+
+
+
+
+            writelines(str, file_path);
 
         end
 
-        function obj = parse_avl(results_path)
+
+
+
+
+        function obj = parse_avl(saved_files)
 
         end
 
@@ -62,7 +153,10 @@ classdef Vehicle
 
     methods (Static)
 
-        function results = run_avl(cmd_file)
+
+        function run_avl(cmd_file)
+
+            system(sprintf('cd "+lib\+AVL" && avl < "%s"', cmd_file)); 
 
         end
 
