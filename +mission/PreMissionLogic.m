ab7f4b69a680ee33e0156cb0e8b1ccccf3aef699
RGHman <rgholman.2004@gmail.com>
2025-08-28 15:09:45 -0500

    Update PreMissionLogic.m

--- +mission/PreMissionLogic.m
+++ +mission/PreMissionLogic.m
@@ -16,10 +16,7 @@
 % - Have hard coded values like ground friction coefficient and nVals
 % - logic that differentiates tricycle gear configs (aoaSet, vRot, etc)
 
-% aoaSet = The aoa at forward ground run. Often determined by the landing
-% gear configuration and the wing incidence angle. For tricycle gear and 0
-% wing incidence, this is probably 0. For tail dragger, this is often the
-% aoaLOF because the takeoff rotation segment doesn't exist.
+
 
 % aoaLOF = The angle of attack for takeoff
 aoaLOF = (CL_TO - CL0) / CL_Alpha;
@@ -29,27 +26,41 @@ weight = mass * g;
 vStall = sqrt(weight*2/(Sref * rho * CL_Max));
 vLOF = 1.3 * vStall;
 
-% vROT = The speed at which elevator authority starts. Literally where the
+% aoaSet = The aoa at forward ground run
+if TailDragger == 1
+    aoaSet = aoaLOF;
+else
+    aoaSet = 0;
+end
+
+% vROT = The speed at which elevator authority starts. Where the
 % moment about the rear gear is above zero. Need distance that CG is
 % forward of rear gear, the aircraft weight, thrust, centerline height
 % above ground, and dCL/dE elevator effectiveness or d_alpha/dE and
 % dCL/d_alpha for the elevator. vRot = vLOF for tail draggers.
-
-% vClimb = The optimal airspeed to climb at. It is the point (airspeed) at which
-% maximum available power/thrust occurs.
-
-% hDotClimb = from vClimb, determine the hDot that would be achieved at
-% vClimb
-aoaTest = linspace(0, aoaMax, nVals);
 vTest   = linspace(0, vMax, nVals);
+M_about_rear = zeros(1, nVals);
+for i = 1 : nVals
+    M_about_rear = F_HT*(tail_arm - d_gear_to_quarter_chord)...
+                    - thrust * h_thrust - weight * d_gear_to_CG;
+end
+
+aoaTest = linspace(0, aoaMax, nVals);
+hDot = zeros(1, nVals^2);
+aoa  = zeros(1, nVals^2);
+v    = zeros(1, nVals^2);
 for i = 1 : nVals % aoa iterations
-    aoa = aoaTest(i);
-    CL = CL0 + CL_Alpha * aoa;
-    CD = CD0 + k*CL^2;
     for j = 1 : nVals % v iterations
-        v = vTest(j);
-        thrust = ThrustCalc(v);
-        hDot   = (thrust - 0.5*rho*v^2 * CD * Sref)*v / weight;
-        vGround 
+        aoa(i*(j-1) + j) = aoaTest(i);
+        CL = CL0 + CL_Alpha * aoaTest(i);
+        CD = CD0 + k*CL^2;
+        v(i*(j-1) + j) = vTest(j);
+        thrust = ThrustCalc(vTest(j));
+        hDot(i*(j-1) + j)   = (thrust - 0.5*rho*v^2 * CD * Sref)*v / weight;
+        vGround = sqrt(vTest(j)^2 - hDot(i*(j-1) + j)^2);
     end
 end
+
+[max_climb_hDot, max_climb_idx] = max(hDot);    % best climb rate
+max_climb_v = v(max_climb_idx);                 % best airspeed for climb
+max_climb_aoa = aoa(max_climb_idx);             % best aoa for climb
