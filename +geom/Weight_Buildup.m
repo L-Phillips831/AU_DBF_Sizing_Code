70180058f723d15bdc412a167009f685c6fe8a28
RGHman <rgholman.2004@gmail.com>
2025-09-06 12:36:28 -0500

    Update Weight_Buildup.m

--- +geom/Weight_Buildup.m
+++ +geom/Weight_Buildup.m
@@ -1,4 +1,4 @@
-%% WeightBuildup %%
+classdef Weight_Buildup < geom.Component
 %{
 Description:
 A payload and battery based MTOW buildup. Empty weight is an implicit
@@ -12,59 +12,73 @@ energy, payload/scoring details passed in, and select the mission being
 ran. It is good practice to estimate weight for the heaviest mission for
 each battery energy iteration
 %}
+    properties
+        Energy (1,1) double
+        Flag (1,1) double
+        Ducks (1,1) double
+        Pucks (1,1) double
+        Length (1,1) double
+    end
 
-% Inputs:
-% E_Battery [Wh]
-% num_Ducks
-% num_Pucks
-% length_Banner [m]
-% Mission_Flag
-% (E_Battery, Mission_Flag, num_Ducks, num_Pucks, L_Banner)
+    methods
+        function obj = Weight_Buildup(Energy, Mission, Ducks, Pucks, Length)
+            obj.Energy = Energy;
+            obj.Mission = Mission;
+            obj.Ducks = Ducks;
+            obj.Pucks = Pucks;
+            obj.Length = Length;
+        end
 
-% Outputs:
-% W0
-% W_Empty
-% W_Battery
-% W_Payload
-% (W0, W_Empty, W_Battery, W_Payload)
+        function obj = get_weight(obj)
+            E_Battery = obj.Energy; % [Wh]
+            Mission_Flag = obj.Mission; % Mission 1,2,3
+            num_Ducks = obj.Ducks;
+            num_Pucks = obj.Pucks;
+            L_Banner = obj.Length; % [m]
 
+            % To do:
+            % - Add a better description of method
+            % - Get Logan's formatting
 
-% To do:
-% - Add a better description of method
-% - Get Logan's formatting
+            W0 = 7; % [kg]
+            A = 5.098; % Based on previous years
+            C = -0.791; % Based on previous years
+            rho_Battery = 155; % [Wh/kg]
+            rho_Banner = 0.08; % [kg/m^2], approximation for light nylon and mesh
+            mass_Puck = 0.163; % [kg/puck]
+            mass_Duck = 0.017; % [kg/duck]
 
-W0 = 7; % [kg]
-A = 5.098; % Based on previous years
-C = -0.791; % Based on previous years
-rho_Battery = 155; % [Wh/kg]
-rho_Banner = 0.08; % [kg/m^2], approximation for light nylon and mesh
-mass_Puck = 0.163; % [kg/puck]
-mass_Duck = 0.017; % [kg/duck]
+            % Battery Weight
+            W_Battery = E_Battery / rho_Battery;
 
-% Battery Weight
-W_Battery = E_Battery / rho_Battery;
+            % Payload Weight
+            switch Mission_Flag
+                case 2 % Mission 2 payload summation
+                    W_Payload = mass_Duck * num_Ducks + mass_Puck * num_Pucks;
+                case 3 % Mission 3 payload summation
+                    W_Payload = 0.2 * L_Banner^2 * rho_Banner;
+                otherwise
+                    W_Payload = 0;
+            end
 
-% Payload Weight
-switch Mission_Flag
-    case 2 % Mission 2 payload summation
-        W_Payload = mass_Duck * numDucks + mass_Puck * num_Pucks;
-    case 3 % Mission 3 payload summation
-        W_Payload = 0.2 * L_Banner^2 * rho_Banner;
-    otherwise
-        W_Payload = 0;
-end
+            % Empty Weight
+            % Note: Includes attached banner mechanisms and M2 payload securement
+            for i = 1:100
+                W_Empty_Frac = A*W0^C;
+                W_Empty = W_Empty_Frac * W0;
+                W0_Prev = W0;
+                W0 = W_Empty + W_Payload + W_Battery;
+                if abs((W0_Prev - W0)/W0) < 0.001
+                    break;
+                end
+            end
 
-% Empty Weight
-% Note: Includes attached banner mechanisms and M2 payload securement
-for i = 1:100
-    W_Empty_Frac = A*W0^C;
-    W_Empty = W_Empty_Frac * W0;
-    W0_Prev = W0;
-    W0 = W_Empty + W_Payload + W_Battery;
-    if abs((W0_Prev - W0)/W0) < 0.001
-        break;
+            W0 = W_Empty + W_Payload + W_Battery;
+
+            obj.W0 = W0;
+            obj.W_Empty = W_Empty;
+            obj.W_Payload = W_Payload;
+            obj.W_Battery = W_Battery;
+            end
     end
 end
-
-W0 = W_Empty + W_Payload + W_Battery;
-
