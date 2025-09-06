71942ffaaa8957dafcf6d0e9911fef7a5b4b1496
RGHman <rgholman.2004@gmail.com>
2025-09-06 12:04:49 -0500

    Update Weight_Buildup.m

--- +geom/Weight_Buildup.m
+++ +geom/Weight_Buildup.m
@@ -1,46 +1,69 @@
 %% WeightBuildup %%
 %{
 Description:
-A physics-based weight build-up that takes aircraft sizing and loading
-parameters and estimates the total a/c mass. Each component (wing, tail,
-fuselage, etc) will have its respective mass approximation method.
-Part-by-part weight buildup is utilized with adjustment factors to find
-component masses. Adjustment factors account for the small inaccuracies
-between these method's estimates and the true fabricated masses. This
-buildup, for now, is suited for conventional configuration aircraft
-%}
+A payload and battery based MTOW buildup. Empty weight is an implicit
+function of historical competition exponential constants and MTOW. Battery
+weight is a function of battery energy and energy density averaged across
+several suppliers and energy amounts.
 
-% W0 = W_Empty + W_Payload + W_Fuel
-% W_Empty = A*W0_Guess^C; Historical Data Implicit Calculation
-% W_Fuel = W_Battery
-% W_PL = f(ducks,pucks, payload)
+Intended Use:
+During iteration of energy consumption across functions, input the battery
+energy, payload/scoring details passed in, and select the mission being 
+ran. It is good practice to estimate weight for the heaviest mission for
+each battery energy iteration
+%}
 
 % Inputs:
 % E_Battery [Wh]
 % num_Ducks
 % num_Pucks
 % length_Banner [m]
-% W0_Guess [kg]
+% Mission_Flag
+% (E_Battery, Mission_Flag, num_Ducks, num_Pucks, L_Banner)
+
+% Outputs:
+% W0
+% W_Empty
+% W_Battery
+% W_Payload
+% (W0, W_Empty, W_Battery, W_Payload)
+
 
 % To do:
-% - Make convergence logic for empty weight
-% - Add flags for M1, M2, or M3
 % - Add a better description of method
 % - Get Logan's formatting
 
-A = 2.36;
-C = -0.18;
+W0 = 7; % [kg]
+A = 5.098; % Based on previous years
+C = -0.791; % Based on previous years
 rho_Battery = 155; % [Wh/kg]
+rho_Banner = 0.08; % [kg/m^2], approximation for light nylon and mesh
+mass_Puck = 0.163; % [kg/puck]
+mass_Duck = 0.017; % [kg/duck]
 
 % Battery Weight
 W_Battery = E_Battery / rho_Battery;
 
 % Payload Weight
+switch Mission_Flag
+    case 2 % Mission 2 payload summation
+        W_Payload = mass_Duck * numDucks + mass_Puck * num_Pucks;
+    case 3 % Mission 3 payload summation
+        W_Payload = 0.2 * L_Banner^2 * rho_Banner;
+    otherwise
+        W_Payload = 0;
+end
 
 % Empty Weight
-for i = 1:20
-    W_Empty = A*W0^C;
+% Note: Includes attached banner mechanisms and M2 payload securement
+for i = 1:100
+    W_Empty_Frac = A*W0^C;
+    W_Empty = W_Empty_Frac * W0;
+    W0_Prev = W0;
     W0 = W_Empty + W_Payload + W_Battery;
+    if abs((W0_Prev - W0)/W0) < 0.001
+        break;
+    end
 end
 
 W0 = W_Empty + W_Payload + W_Battery;
