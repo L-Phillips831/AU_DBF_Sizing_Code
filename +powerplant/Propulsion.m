e5e469ed59f09bc17d9eeb79c935f1e70e93939f
L-Phillips831 <loganaphillips831@gmail.com>
2025-09-07 17:01:15 -0500

    Added GetThrust method.

--- +powerplant/Propulsion.m
+++ +powerplant/Propulsion.m
@@ -13,6 +13,14 @@ classdef Propulsion
             Prop.propeller = propeller;
             Prop.motor = Prop.motor.calcMaxPower(Prop.battery);
         end
+
+        function thrust = get_Thrust(Prop, throt_setting, airspeed)
+            RPM = Propulsion.calcRPM(throt_setting);
+            J = airspeed / ((RPM / 60) * Prop.propeller.diameter);
+
+            thrust = Prop.propeller.GI_Thrust_fcn_J_RPM(J, RPM);
+
+        end
     end
 
     methods (Static)
