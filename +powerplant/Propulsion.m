5046c9314ae8ef0a7720f10a618e7ebc1c1d58e3
L-Phillips831 <loganaphillips831@gmail.com>
2025-09-08 09:54:37 -0500

    Manual addition of updates to propulsion.

--- +powerplant/Propulsion.m
+++ +powerplant/Propulsion.m
@@ -1,32 +1,59 @@
 classdef Propulsion
     properties
-        battery (1,:) powerplant.battery
-        esc powerplant.esc
-        motor powerplant.motor
-        propeller powerplant.propeller
+        battery (1,:) battery
+        esc (1,:) esc
+        motor (1,:) motor
+        propeller (1,:) propeller
+        wire (1,:) wiring
     end
     methods
-        function Prop = Propulsion(battery,esc,motor,propeller)
-            Prop.battery = battery;
-            Prop.esc = esc;
-            Prop.motor = motor;
-            Prop.propeller = propeller;
-            Prop.motor = Prop.motor.calcMaxPower(Prop.battery);
+        function prop = Propulsion(battery,esc,motor,propeller,wire)
+            prop.battery = battery;
+            prop.esc = esc;
+            prop.motor = motor;
+            prop.propeller = propeller;
+            prop.wire = wire;
+            prop.motor = prop.motor.calcMaxPower(prop.battery);
         end
 
-        function thrust = get_Thrust(Prop, throt_setting, airspeed)
-            RPM = Propulsion.calcRPM(throt_setting);
-            J = airspeed / ((RPM / 60) * Prop.propeller.diameter);
+        function RPM = calcRPM(prop, throttle_setting)
 
-            thrust = Prop.propeller.GI_Thrust_fcn_J_RPM(J, RPM);
+           RPM = prop.motor.Kv .* prop.battery.voltage .* throttle_setting;
+     
+        end
 
+        function efficiency = calcMotorEfficiency(prop,powerOut)
+            current = prop.battery.voltage ./ powerOut;
+            powerIn = powerOut + (prop.motor.Io * prop.battery.voltage) + (prop.motor.Rm .* current.^2);
+
+            efficiency = powerOut ./ powerIn;
+        end
+
+        function efficiency = calcWireEfficiency(prop,powerOut)
+            current = prop.battery.voltage ./ powerOut;
+            powerIn = powerOut + (prop.wire.Rm .* current.^2);
+
+            efficiency = powerOut ./ powerIn;
+        end
+
+        function powerIn = calcReqiuredPower(prop,RPM,V)
+            powerOut = prop.propeller.GI_Power_fcn_V_RPM(V,RPM);
+
+            powerOut(isnan(powerOut)) = 0;
+            motorEfficiency = prop.calcMotorEfficiency(powerOut);
+
+            powerIntoMotor = powerOut ./ (motorEfficiency + 1e-10);
+            wireEfficiency = prop.calcWireEfficiency(powerIntoMotor);
+            
+            powerIn = powerIntoMotor ./ (wireEfficiency + 1e-10);
+        end
+
+        function Thrust = calcThrust(prop,RPM,V)
+            Thrust = prop.propeller.GI_Thrust_fcn_V_RPM(V,RPM);
         end
     end
 
     methods (Static)
         
-        function RPM = calcRPM(Prop,ThrottleSetting)
-            RPM = Prop.motor.Kv .* Prop.battery.voltage .* ThrottleSetting;
-        end
     end
 end
--- +powerplant/propeller.m
+++ +powerplant/propeller.m
@@ -4,9 +4,10 @@ classdef propeller
 
     properties
         perfData (1,:) cell % Cell array of tables containing propellor performance data
-        GI_Thrust_fcn_J_RPM griddedInterpolant = griddedInterpolant(1:2,1:2) % Gridded interpolant to calculate Thrust {N} from J and RPM
-        GI_Power_fcn_J_RPM griddedInterpolant = griddedInterpolant(1:2,1:2) % Gridded interpolant to calculate Power reqiured {W} from J and RPM
-        pitch (1,1) double % Propellor pitch
+        GI_Thrust_fcn_V_RPM griddedInterpolant = griddedInterpolant(1:2,1:2) % Gridded interpolant to calculate Thrust {N} from V {m/s} and RPM
+        GI_Power_fcn_V_RPM griddedInterpolant = griddedInterpolant(1:2,1:2) % Gridded interpolant to calculate Power reqiured {W} from V {m/s} and RPM
+        GI_RPM_fcn_V_Thrust griddedInterpolant = griddedInterpolant(1:2,1:2) % Gridded interpolant to calculate RPM reqiured from V {m/s} and Thrust reqiured {N}
+        pitch (1,1) double % Propellor pitch {in}
         diameter (1,1) double % Propellor diameter {m}
     end
 
@@ -31,7 +32,7 @@ classdef propeller
             % ".dat" files to a cell array of matlab tables.
 
             % Initialize counters and flags
-            TableFlag = 0;
+            TableFlag = false;
             n = 0;
             i = 1;
 
@@ -39,8 +40,7 @@ classdef propeller
             Data = zeros(30,15,50);
             
             % Open .dat file and start reading line by line
-            perfDataFile
-            fid = fopen(perfDataFile)
+            fid = fopen(perfDataFile);
             tline = fgetl(fid);
             
             % Loop until a line has no characters in it. This should be end
@@ -52,11 +52,11 @@ classdef propeller
                 elseif contains(tline,"RPM =") % If a line contains "RPM =" sets flag to grab data on next line
                     i = i+1;
                     n = n+1;
-                    TableFlag = 1;
-                elseif TableFlag == 1 % Gets prop data and resets TableFlag
+                    TableFlag = true;
+                elseif TableFlag == true % Gets prop data and resets TableFlag
             
                     Data(:,:,i-1) = readmatrix(perfDataFile,"Range",string(n+5)+":"+string(n+35));
-                    TableFlag = 0;
+                    TableFlag = false;
                     n = n+1;
                 else
                     n = n+1;
@@ -74,8 +74,6 @@ classdef propeller
                     'Thrust_lbf','PWR_W','Torque_Nm','Thrust_N','THR2PWR_g2W','Mach','Reyn','FOM'};
                 perfData{j}.Properties.UserData.RPM = j*1000; % Sets custom table RPM property for later reference
             end
-
-            fclose(fid);
         end
     
     end
@@ -83,46 +81,81 @@ classdef propeller
     methods(Access=private)
         function prop = mkGriddedInterps(prop)
             % Creates gridded interpolant for thrust {N} and power {W} as a function of
-            % advance ratio (j) and RPM.
+            % velocity {m/s} and RPM.
 
-            % Loop through all advance ratios and get maximum value.
-            Jmax = 0;
+            % Loop through all velocities and get maximum value.
+            Vmax = 0;
             for i=1:length(prop.perfData)
-                Jtest = max(prop.perfData{i}.J);
-                if Jtest > Jmax
-                    Jmax = Jtest;
+                Vtest = max(prop.perfData{i}.V_mph) * 0.44704;
+                if Vtest > Vmax
+                    Vmax = Vtest;
                 end
             end
 
-            % Create vector of common advance ratios and RPM values
-            Jvec = linspace(0,Jmax,100)';
+            % Create vector of common velocities and RPM values
+            Vvec = linspace(0,Vmax,100)';
             RPMvec = (1:length(prop.perfData)) * 1000;
 
             % Convert vectors to matrices
-            Jmat = repmat(Jvec,1,length(RPMvec));
-            RPMmat = repmat(RPMvec,length(Jvec),1);
+            Vmat = repmat(Vvec,1,length(RPMvec));
+            RPMmat = repmat(RPMvec,length(Vvec),1);
             
             % Initialize Thrust and Power matrices
-            Tmat = zeros(length(Jvec),length(prop.perfData));
-            PWRmat = zeros(length(Jvec),length(prop.perfData));
+            Tmat = zeros(length(Vvec),length(prop.perfData));
+            PWRmat = zeros(length(Vvec),length(prop.perfData));
             
             % Get values for Thrust and PWR at each advance ratio and RPM
             for i=1:length(prop.perfData)
                 % Remove any NaN from data and interpolate to generate new data
-                J = prop.perfData{i}.J(~isnan(prop.perfData{i}.Thrust_N));
+                V = prop.perfData{i}.V_mph(~isnan(prop.perfData{i}.Thrust_N)) * 0.44704;
                 T = prop.perfData{i}.Thrust_N(~isnan(prop.perfData{i}.Thrust_N));
                 P = prop.perfData{i}.PWR_W(~isnan(prop.perfData{i}.Thrust_N));
 
-                Tmat(:,i) = interp1(J,T,Jvec,'spline','extrap');
-                PWRmat(:,i) = interp1(J,P,Jvec,'spline','extrap');
+                Tmat(:,i) = interp1(V,T,Vvec,'linear','extrap');
+                PWRmat(:,i) = interp1(V,P,Vvec,'linear','extrap');
             end
-            % Make any negative values in the pwoer matrix 0
+            % Make any negative values in the power matrix 0
             PWRmat(Tmat<0) = 0;
             
             % Make gridded interpolants from matrices
-            prop.GI_Thrust_fcn_J_RPM = griddedInterpolant(Jmat,RPMmat,Tmat,'linear','linear');
-            prop.GI_Power_fcn_J_RPM = griddedInterpolant(Jmat,RPMmat,PWRmat,'linear','nearest');
+            prop.GI_Thrust_fcn_V_RPM = griddedInterpolant(Vmat,RPMmat,Tmat,'linear','linear');
+            prop.GI_Power_fcn_V_RPM = griddedInterpolant(Vmat,RPMmat,PWRmat,'linear','nearest');
+
+            % Make mirrored GI for getting RPM for a given thrust reqiured
+            prop = prop.mkRPMGriddedInterp;
             
+        end
+
+        function prop = mkRPMGriddedInterp(prop)
+            % Creates a GI for RPM as a function of velocity {m/s}
+            % and thrust reqiured {N}
+            
+            % get the maximum thrust value
+            maxThrust = max(max(prop.GI_Thrust_fcn_V_RPM.Values));
+            
+            % create vector of common velocities and thrusts
+            Tvec = linspace(0,maxThrust,50);
+            Vvec = prop.GI_Thrust_fcn_V_RPM.GridVectors{1,1}';
+            
+            % convert vectors to matrices
+            Vmat = repmat(Vvec,1,length(Tvec));
+            Tmat = repmat(Tvec,length(Vvec),1);
+
+            % get RPM test vector and initialize RPM matrix
+            RPMvec = prop.GI_Thrust_fcn_V_RPM.GridVectors{1,2};
+            RPMmat = zeros(length(Vvec),length(Tvec));
+
+            % get rpm value for each velocity and thrust pair
+            for i=1:length(Vvec)
+                % get thrust values aligned with RPM test vector
+                T = prop.GI_Thrust_fcn_V_RPM(Vvec(i).*ones(1,length(RPMvec)),RPMvec);
+
+                % get RPM for the common thrust value
+                RPMmat(i,:) = interp1(T,RPMvec,Tvec,'linear');
+            end
+
+            % create gridded interpolants from matrices
+            prop.GI_RPM_fcn_V_Thrust = griddedInterpolant(Vmat,Tmat,RPMmat,'linear','linear');
         end
 
     end
