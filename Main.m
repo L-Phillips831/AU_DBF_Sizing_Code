6b87f9c915fa26b892f3c1f89c21b350efcc7c60
L-Phillips831 <loganaphillips831@gmail.com>
2025-09-21 20:27:10 -0500

    Finalized initial mission passing. Beginning first pass GA optimization.

--- Main.m
+++ Main.m
@@ -8,15 +8,15 @@ clc, clear, close all;
 
 %% Create the Optimizer Object
 
-ga_settings.lower_bds = [5*0.3048, 17, 4, 0];                % Set the lower bounds for the optimizer
-ga_settings.upper_bds = [30, 30, 8, 10];               % Set the upper bounds for the  optimizer
+ga_settings.lower_bds = zeros([1 3]);                % Set the lower bounds for the optimizer
+ga_settings.upper_bds = [100 100 100];               % Set the upper bounds for the  optimizer
 ga_settings.pop_size = 15;                           % Set the population size for GA optimizer
 ga_settings.generations = 5;                         % Define the number of generations for the GA optimizer
 GA_optimizer = optimizer.GA_Optimizer(ga_settings);  % Create optimizer object       
 
 
-[best_score, best_params] = GA_optimizer.run(@MOG_Solver);
-% MOG_Solver(10*0.3048, 20, 4.4, 3);
+% [best_score, best_params] = GA_optimizer.run(@MOG_Solver);
+MOG_Solver(10*0.3048, 5.16, 4.4, 3);
 
 
 
@@ -30,13 +30,7 @@ GA_optimizer = optimizer.GA_Optimizer(ga_settings);  % Create optimizer object
 %% Running the MOG Solver
 
 
-function cost = MOG_Solver(input_arr)
-
-    %%% Extract inputs
-    banner_length    = input_arr(1);
-    W_S              = input_arr(2);
-    AR               = input_arr(3);
-    num_pucks        = ceil(input_arr(4));
+function cost = MOG_Solver(banner_length, W_S, AR, num_pucks)
 
     %%% Define Vehicle
     aircraft = Vehicle("Aircraft", W_S, num_pucks, banner_length, 0.7, 0.05);
@@ -72,180 +66,70 @@ function cost = MOG_Solver(input_arr)
     aircraft.add_propulsion(bat_cells, bat_Wh, motor_kv, motor_max_current, prop_pitch, prop_diam);
 
     aircraft.analyze_airframe()
+    aircraft.get_banner_drag(100 * 0.3048)
     
 
     %%% Define Missions
-    flag_dist = 500 * 0.3048;
-    vWind = [0 0 0];
-
-    [mission_1_t, mission_1_E, tab] = run_mission_1(aircraft, flag_dist, vWind);
-    [mission_2_t, mission_2_E, tab, m2_laps] = run_mission_2(aircraft, flag_dist, vWind);
-    [mission_3_t, mission_3_E, tab, m3_laps] = run_mission_3(aircraft, flag_dist, vWind);
-
-
-    mission_1_E = 0;
-
-    %%% Gather Total Performance
-    max_E = max([mission_1_E, mission_2_E, mission_3_E]) / 3600;
-    if max_E > 100
-        cost = (max_E - 100) * 1e6;
-        fprintf("Aircraft failed to meet 100 Wh limit. Total Energy usage of %.3f\n", max_E);
-        fprintf("Setting cost to %.3e\n", cost);
-        return;
-    end
-
-    safety_margin = 1.2;
-
-    aircraft.battery_capacity
-    while (abs(max_E*safety_margin - aircraft.battery_capacity) > 1)
-        fprintf("Current Solved Energy is %.3f Wh. Resizing.\n", max_E);
-        aircraft.resize_geom(max_E*safety_margin);
-        
-
-        [mission_1_t, mission_1_E, tab] = run_mission_1(aircraft, flag_dist, vWind);
-        [mission_2_t, mission_2_E, tab, m2_laps] = run_mission_2(aircraft, flag_dist, vWind);
-        [mission_3_t, mission_3_E, tab, m3_laps] = run_mission_3(aircraft, flag_dist, vWind);
-
-
-        mission_1_E = 0;
-
-        %%% Gather Total Performance
-        max_E = max([mission_1_E, mission_2_E, mission_3_E]) / 3600;
-        if max_E > 100
-            cost = (max_E - 100) * 1e6;
-            fprintf("Aircraft failed to meet 100 Wh limit. Total Energy usage of %.3f\n", max_E);
-            fprintf("Setting cost to %.3e\n", cost);
-            return;
-        end
-
-        
-
-    end
-
-    fprintf("Design Converged at %.3f Wh.\n", max_E);
-
-
-
-    % Generate Cost
-    % To do: find missing variables, create a ground mission function
-    % Logan checks: span call on RAC calculation, ducks call
-    % Missing: num_laps_2, num_laps_3
-    % num_laps_3
-    score_GM_Norm = 10;
-    time_run = 1.5; % seconds
-    time_load_M2 = 2*ceil(aircraft.num_pucks/4) + ceil(2.5*aircraft.num_ducks/6); % fix
-    time_unload_M2 = 2*ceil(aircraft.num_pucks/4) + ceil(aircraft.num_ducks/4); % fix
-    time_load_M3 = 2; % seconds
-    score_GM_AU = 4*time_run + time_load_M2 + time_unload_M2 + time_load_M3;
-    score_GM = score_GM_Norm/score_GM_AU;
-
-    get_income = @(n_Pass, n_Cargo, n_Laps) ...
-        (n_Pass*(6+(2*n_Laps))) + (n_Cargo * (10 + (8*n_Laps)));
-    get_cost = @(n_Laps, n_Pass, n_Cargo, EF) ...
-        n_Laps*(10+(n_Pass * 0.5) + (n_Cargo * 2)) * EF;
-    income_Norm = get_income(60, 20, 10); % 60 ducks, 20 pucks, 10 laps
-    cost_Norm = get_cost(10, 60, 20, 1);
-    score_M2_Norm = income_Norm - cost_Norm;
-    income_AU = get_income(aircraft.num_ducks, aircraft.num_pucks, m2_laps);
-    EF = aircraft.battery_capacity / 100;
-    cost_AU = get_cost(m2_laps, aircraft.num_ducks, aircraft.num_pucks, EF);
-    score_M2_AU = income_AU - cost_AU;
-    score_M2 = score_M2_AU/score_M2_Norm;
-
-    score_M3_Norm = 35*8/0.9; % 35 ft banner, 0.9 RAC, 8 laps
-    RAC = max(0.9, 0.05*aircraft.b_ref + 0.75);
-    score_M3_AU = banner_length*m3_laps*RAC;
-    score_M3 = score_M3_AU/score_M3_Norm;
-
-    score = score_GM + score_M2 + score_M3;
-    cost = 1/score * 100; % Higher score generates lower cost.
-
-    fprintf("Generated cost: %.3f\n", cost);
-
-end
-
-
-
-
-
-
-
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MISSION DEFINITIONS %%%%%%%%%%%%%%%%%%%%%%%%
-function [t_final, E_final, tab] = run_mission_1(aircraft, flag_dist, vWind)
 
     % % Mission 1  **EXAMPLE**
-    mission_1_laps = 3;
-    mission_1_steps = 50;
-    mission_1 = mission.Mission_Builder(mission_1_laps, aircraft, mission_1_steps, vWind, 'mission 1');
+    % mission_1_laps = 3;
+    % mission_1_steps = 50;
+    % mission_1_cruise_alt = 200;
+    % mission_1_throttle = 0.6;
+    % mission_1_flag_dist = 500;
+    % 
+    % mission_1 = mission.Mission_Builder(mission_1_laps, aircraft, mission_1_steps);
+    % 
+    % % Lap 1
+    % mission_1.add_takeoff();
+    % mission_1.add_climb(mission_1_cruise_alt, mission_1_throttle, 200);
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % mission_1.add_turn(180, 'instantaneous');
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % mission_1.add_turn(360, 'sustained');
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % mission_1.add_turn(180, 'instantaneous');
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % 
+    % % Lap 2
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % mission_1.add_turn(180, 'instantaneous');
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % mission_1.add_turn(360, 'sustained');
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % mission_1.add_turn(180, 'instantaneous');
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % 
+    % % Lap 3
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % mission_1.add_turn(180, 'instantaneous');
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % mission_1.add_turn(360, 'sustained');
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % mission_1.add_turn(180, 'instantaneous');
+    % mission_1.add_cruise(mission_1_flag_dist/2, mission_1_throttle);
+    % % somehow land...
+    % 
+    % [t_total, E_total] = mission_1.run();
 
 
 
-    % Lap 1
-    m1_takeoff_throttle = 0.7;
-    m1_climb_throttle = 0.7;
-    mission_1.add_takeoff(m1_takeoff_throttle, m1_climb_throttle);
-
-    m1_climb_h = 200 * 0.3048;
-    mission_1.add_climb(m1_climb_h, m1_climb_throttle);
-    [~, ~, tab] = mission_1.run();
-    mission_1.clear();
-
-    dist_traveled = tab.Position_NED(end,1);
-    m1_lap1_cruise = flag_dist/2 - dist_traveled;
-    m1_cruise_throttle = 0.65;
-    m1_turn_throttle = 0.8;
-    mission_1.add_cruise(m1_lap1_cruise, m1_cruise_throttle);
-    mission_1.add_turn(m1_turn_throttle, 180, 'sustained');
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-    mission_1.add_turn(m1_turn_throttle, 360, 'sustained');
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-    mission_1.add_turn(m1_turn_throttle, 180, 'sustained');
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-
-    % Lap 2 
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-    mission_1.add_turn(m1_turn_throttle, 180, 'sustained');
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-    mission_1.add_turn(m1_turn_throttle, 360, 'sustained');
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-    mission_1.add_turn(m1_turn_throttle, 180, 'sustained');
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-
-    % Lap 3
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-    mission_1.add_turn(m1_turn_throttle, 180, 'sustained');
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-    mission_1.add_turn(m1_turn_throttle, 360, 'sustained');
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-    mission_1.add_turn(m1_turn_throttle, 180, 'sustained');
-    mission_1.add_cruise(flag_dist/2, m1_cruise_throttle);
-    % somehow land...
-
-    [t_final, E_final, tab] = mission_1.run(tab);
-    
-   
-
-end
-
-function [t_final, E_final, tab, laps] = run_mission_2(aircraft, flag_dist, vWind)
-
-%%% Mission 2
+    %%% Mission 2
+    flag_dist = 500 * 0.3048;
     mission_2_laps = 3;
     mission_2_steps = 50;
+    vWind = [0 0 0];
     mission_2 = mission.Mission_Builder(mission_2_laps, aircraft, mission_2_steps, vWind, 'mission 2');
 
     % Lap 1
     m2_laps = 0;
     m2_takeoff_setting = 0.9;
-    m2_climb_throttle = 0.9;
+    m2_climb_throttle = 0.8;
     mission_2.add_takeoff(m2_takeoff_setting, m2_climb_throttle);
 
     m2_climb_h = 200 * 0.3048;
     mission_2.add_climb(m2_climb_h, m2_climb_throttle);
-    [~, ~, tab] = mission_2.run();
+    [t_final, ~, tab] = mission_2.run()
     mission_2.clear();
 
     dist_traveled = tab.Position_NED(end,1);
@@ -265,7 +149,7 @@ function [t_final, E_final, tab, laps] = run_mission_2(aircraft, flag_dist, vWin
     [t_final, ~, tab] = mission_2.run(tab);
     mission_2.clear();
 
-    m2_laps = m2_laps + 1;
+
    while (true)
         prev_tab = tab;
         last_t = t_final;
@@ -294,83 +178,69 @@ function [t_final, E_final, tab, laps] = run_mission_2(aircraft, flag_dist, vWin
 
   
         m2_laps = m2_laps + 1;
-   end
-
-   laps = m2_laps;
+    end
 
 
-end
+    mission_2_E = E_final;
+    mission_2_t = t_final;
 
-function [t_final, E_final, tab, laps] = run_mission_3(aircraft, flag_dist, vWind)
+
+
+
 
     %%% Mission 3
-    banner_CD_0 = aircraft.get_banner_drag(100 * 0.3048);
-    aircraft.CD_0 = aircraft.CD_0 + banner_CD_0;
 
-    % Lap 1
-    mission_3_laps = 3;
-    mission_3_steps = 50;
-    mission_3 = mission.Mission_Builder(mission_3_laps, aircraft, mission_3_steps, vWind, 'mission 2');
 
-    % Lap 1
-    m3_laps = 0;
-    m3_takeoff_setting = 0.9;
-    m3_climb_throttle = 0.9;
-    mission_3.add_takeoff(m3_takeoff_setting, m3_climb_throttle);
 
-    m3_climb_h = 200 * 0.3048;
-    mission_3.add_climb(m3_climb_h, m3_climb_throttle);
-    [~, ~, tab] = mission_3.run();
-    mission_3.clear();
+    %%% Gather Total Performance
+    max_E = max([mission_1_E, mission_2_E, mission_3_E]);
+    if max_E > 100
+        cost = 10e6;
+    end
 
-    dist_traveled = tab.Position_NED(end,1);
-    m3_lap1_cruise = flag_dist/2 - dist_traveled;
-    m3_cruise_throttle = 0.7;
-    mission_3.add_cruise(m3_lap1_cruise, m3_cruise_throttle);
+    safety_margin = 1.2;
 
-    % Finish the lap
-    m3_turn_throttle = 0.9;
-    mission_3.add_turn(m3_turn_throttle, 180, 'sustained');
-    mission_3.add_cruise(flag_dist/2, m3_cruise_throttle);
-    mission_3.add_turn(m3_turn_throttle, 360, 'sustained');
-    mission_3.add_cruise(flag_dist/2, m3_cruise_throttle);
-    mission_3.add_turn(m3_turn_throttle, 180, 'sustained');
-    mission_3.add_cruise(flag_dist/2, m3_cruise_throttle);
-    
-    [t_final, ~, tab] = mission_3.run(tab);
-    mission_3.clear();
+    while (max_E - aircraft.battery_capacity > 1)
+        aircraft.resize_geom(max_E*safety_margin);
+        mission_1.run();
+        mission_2.run();
+        mission_3.run();
+    end
 
-    m3_laps = m3_laps + 1;
-   while (true)
-        prev_tab = tab;
-        last_t = t_final;
 
-        mission_3.add_cruise(flag_dist/2, m3_cruise_throttle);
-        mission_3.add_turn(m3_turn_throttle, 180, 'sustained');
-        mission_3.add_cruise(flag_dist/2, m3_cruise_throttle);
-        mission_3.add_turn(m3_turn_throttle, 360, 'sustained');
-        mission_3.add_cruise(flag_dist/2, m3_cruise_throttle);
-        mission_3.add_turn(m3_turn_throttle, 180, 'sustained');
-        mission_3.add_cruise(flag_dist/2, m3_cruise_throttle);
-    
-        [t_final, E_final, tab] = mission_3.run(prev_tab);
-        mission_3.clear();
-        
 
-        if abs(t_final - 300) < 1e-6
-            break;
+    % Generate Cost
+    % To do: find missing variables, create a ground mission function
+    % Logan checks: span call on RAC calculation, ducks call
+    % Missing: num_laps_2, num_laps_3
+    % num_laps_3
+    score_GM_Norm = 10;
+    time_run = 1.5; % seconds
+    time_load_M2 = 2*ceil(aircraft.num_pucks/4) + ceil(2.5*aircraft.num_ducks/6); % fix
+    time_unload_M2 = 2*ceil(aircraft.num_pucks/4) + ceil(aircraft.num_ducks/4); % fix
+    time_load_M3 = 2; % seconds
+    score_GM_AU = 4*time_run + time_load_M2 + time_unload_M2 + time_load_M3;
+    score_GM = score_GM_Norm/score_GM_AU;
 
-        elseif t_final > 300
-            % If you fail to complete the lap before 5 min, dont count the time for
-            % scoring
-            t_final = last_t;
-            break;
-        end
+    get_income = @(n_Pass, n_Cargo, n_Laps) ...
+        (n_Pass*(6+(2*n_Laps))) + (n_Cargo * (10 + (8*n_Laps)));
+    get_cost = @(n_Laps, n_Pass, n_Cargo, EF) ...
+        n_Laps*(10+(n_Pass * 0.5) + (n_Cargo * 2)) * EF;
+    income_Norm = get_income(60, 20, 10); % 60 ducks, 20 pucks, 10 laps
+    cost_Norm = get_cost(10, 60, 20, 1);
+    score_M2_Norm = income_Norm - cost_Norm;
+    income_AU = get_income(aircraft.num_ducks, aircraft.num_pucks, num_laps_2);
+    EF = aircraft.battery_capacity / 100;
+    cost_AU = get_cost(num_laps_2, aircraft.num_ducks, aircraft.num_pucks, EF);
+    score_M2_AU = income_AU - cost_AU;
+    score_M2 = score_M2_AU/score_M2_Norm;
 
-  
-        m3_laps = m3_laps + 1;
-   end
+    score_M3_Norm = 35*8/0.9; % 35 ft banner, 0.9 RAC, 8 laps
+    RAC = max(0.9, 0.05*aircraft.b_ref + 0.75);
+    score_M3_AU = banner_length*num_laps_3*RAC;
+    score_M3 = score_M3_AU/score_M3_Norm;
 
-   laps = m3_laps;
+    score = score_GM + score_M2 + score_M3;
+    cost = 1/score; % Higher score generates lower cost.
 
 end
